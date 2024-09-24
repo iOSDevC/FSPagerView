934e2bb7bbb222300b91f6a19fafcc7e6ddb6a3b
Cristopher Bautista <cristopher.bautista@evertecinc.com>
2024-09-24 11:44:52 -0600

    Avoid % numberOfItems if zero

--- Sources/FSPagerView.swift
+++ Sources/FSPagerView.swift
@@ -414,7 +414,7 @@ open class FSPagerView: UIView,UICollectionViewDataSource,UICollectionViewDelega
     }
     
     public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        if self.collectionViewLayout.itemSpacing.isZero == false,
+        if self.collectionViewLayout.itemSpacing.isZero == false, self.numberOfItems != 0,
            let function = self.delegate?.pagerViewWillEndDragging(_:targetIndex:) {
             let contentOffset = self.scrollDirection == .horizontal ? targetContentOffset.pointee.x : targetContentOffset.pointee.y
             let targetItem = lround(Double(contentOffset/self.collectionViewLayout.itemSpacing))
